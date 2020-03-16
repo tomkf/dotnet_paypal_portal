5191c9a29ef648ea2834b755c3af954d514b0c19
tomkf <thomaskeithferris@gmail.com>
2020-03-15 17:34:13 -0700

    config

--- paypal_final/Controllers/HomeController.cs
+++ paypal_final/Controllers/HomeController.cs
@@ -4,26 +4,51 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using paypal_final.Data;
 using paypal_final.Models;
 
 namespace paypal_final.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext _context;
+
+        // Constructor receives the context through dependency injection.
+        public HomeController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        // Home page shows list of items. Item price is set through ViewBag.
         public IActionResult Index()
         {
-            return View();
+            ViewBag.TotalPrice = "3.55";
+            var items = _context.IPNs;
+            return View(items);
         }
 
-        public IActionResult Privacy()
+        // This method receives and stores the Paypal transaction details.
+        [HttpPost]
+        public JsonResult PaySuccess([FromBody]IPN ipn)
         {
-            return View();
+            try
+            {
+                _context.IPNs.Add(ipn);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                return Json(ex.Message);
+            }
+            return Json(ipn);
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        // Show transaction detail. 
+        public IActionResult FinishShopping(string paymentID)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            IPN transaction = _context.IPNs.Where(t => t.paymentID == paymentID).FirstOrDefault();
+            return View(transaction);
         }
+
     }
 }
